9458abaa55f6be151f2623410717793eba5dcff4
MrAouiz <math.aouiz@gmail.com>
2019-04-18 16:25:04 +0200

    Main.py commenté

--- Cassiopée/Code/Main.py
+++ Cassiopée/Code/Main.py
@@ -3,19 +3,28 @@ from bs4 import BeautifulSoup
 import urllib.request
 import re
 
-# test = Advisory("https://ics-cert.us-cert.gov/advisories/ICSA-19-106-02")
 
+"""
+ICS-CERT n'accepte pas les requetes ne venant pas de navigateurs,
+on change alors le User-Agent de nos requete pour se faire passer
+pour Mozilla Firefox:
+"""
 class AppURLopener(urllib.request.FancyURLopener):
     version = "Mozilla/5.0"
 
+# Crée une instance d'opener d'url avec le bon User-Agent
 opener = AppURLopener()
+
+
+# Récupération du code html de l'url de la première page des advisories de ICS-CERT
 page = opener.open("https://ics-cert.us-cert.gov/advisories")
 soup = BeautifulSoup(page,'html.parser')
 
-
-advisories = []
+# Récupère les urls de tous les advisories de la première page
 urls = soup.findAll('a',href=re.compile('/advisories/I'))
-for i in range(1):
+
+# Récupère et envoie en bdd les données des 10 premiers advisories
+for i in range(10):
     url = urls[i].get('href')
     url = "https://ics-cert.us-cert.gov"+url
     adv = Advisory(url)
