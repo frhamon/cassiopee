160bdef9f78159f15741513a2bdc4fe2d44dab0e
François HAMON <francois.hamon@telecom-sudparis.eu>
2019-04-19 18:05:56 +0200

    tests décriture en base

--- Cassiopée/Code/Advisory.py
+++ Cassiopée/Code/Advisory.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup
+﻿from bs4 import BeautifulSoup
 import urllib.request
 import re
 import MySQLdb as mdb
@@ -28,12 +28,15 @@ class Advisory:
 
         # Procédure pour ajouter des données en base, ici il s'agit de la table Vendor
         # !! Remplir passwd avec le mot de passe mysql de l'utilisateur !!
-        db = mdb.Connection(host='localhost', db='cassiopee', passwd='', user='root')
+        
+	""" db = mdb.Connection(host='localhost', db='cassiopee', passwd='clochette', user='root')
         c = db.cursor()
         c.execute(""" insert into vendor values(DEFAULT , %s, %s) """, (self.location, self.vendor))
         db.commit()
         c.close()
         db.close()
+	
+	"""
 
 
         # Print toutes les données récupérées de l'advisory (a enlever une fois que la mise en bdd est réussie)
