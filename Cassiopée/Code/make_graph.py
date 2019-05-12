1cf32da787b4189c1c3ae913db3aeec56fa089ba
MrAouiz <math.aouiz@gmail.com>
2019-05-13 00:48:38 +0200

    premier graphe

--- Cassiopée/Code/make_graph.py
+++ Cassiopée/Code/make_graph.py
@@ -4,7 +4,7 @@ import pandas.io.sql as psql
 import matplotlib.pyplot as plt
 import tkinter
 
-db = mdb.Connection(host='localhost', db='cassiopee', passwd='mathaouiz09', user='root', charset='utf8')
+db = mdb.Connection(host='localhost', db='cassiopee', passwd='clochette', user='root', charset='utf8')
 
 query = "select date FROM cve"
 
