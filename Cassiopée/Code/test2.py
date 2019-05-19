62fc952af81919281a84d74d269fbd90a7491fa7
François HAMON <francois.hamon@telecom-sudparis.eu>
2019-05-19 20:56:21 +0200

    hidden password with getpass()

--- Cassiopée/Code/test2.py
+++ Cassiopée/Code/test2.py
@@ -1,10 +1,12 @@
+from getpass import getpass
+
 import MySQLdb as mdb
 import empty_db
 import Main
 import make_graph
 
 
-mdp = input("Veuillez rentrer votre mot de passe mysql: ")
+mdp = getpass(prompt='Veuillez rentrer votre mot de passe mysql: ')
 while(True):
     #empty_db.empty(mdp)
 
@@ -21,7 +23,7 @@ while(True):
             db = mdb.Connection(host='localhost', passwd=mdp, user='root', charset='utf8')
             db.cursor().execute('CREATE DATABASE cassiopee')
 
-            db = mdb.Connection(host='localhost', passwd='mathaouiz09', user='root', db='cassiopee', charset='utf8')
+            db = mdb.Connection(host='localhost', passwd=mdp, user='root', db='cassiopee', charset='utf8')
             cursor = db.cursor()
 
             fd = open("../Modélisation/cassiopee.sql", 'r')
@@ -55,14 +57,14 @@ while(True):
 print("Voici la liste des graphes disponibles: ")
 print("1. Premier graphe")
 print()
-print("Pour afficher un graphe, tapez 'graph i' avec i le numero du graphe")
+print("Pour afficher un graphe, tapez 'i' avec i le numero du graphe")
 print("Pour réafficher la liste des graphes, tapez 'list' ")
 print("Pour quitter, tapez 'quit' ")
 print();print()
 
 while(True):
     cmd = input("Tapez votre commande: ")
-    if(cmd == 'graph 1'):
+    if(cmd == '1'):
         make_graph.graph(mdp)
     elif(cmd == 'quit'):
         break
