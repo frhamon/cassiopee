816993e8d864d9f0e481e9bd7923907313101a4d
MrAouiz <math.aouiz@gmail.com>
2019-05-20 01:16:47 +0200

    interaction utilisateur

--- Cassiopée/Code/application.py
+++ Cassiopée/Code/application.py
@@ -1,9 +1,10 @@
 import MySQLdb as mdb
 import fill_db
 import make_graph
+from getpass import getpass
 
 
-mdp = input("Veuillez rentrer votre mot de passe mysql: ")
+mdp = getpass(prompt="Veuillez rentrer votre mot de passe mysql: ")
 while(True):
 
     try :
@@ -12,12 +13,11 @@ while(True):
         if (e.args[0] == 1045):
             print("Mot de passe incorrect")
             print()
-            mdp = input("Veuillez rentrer votre mot de passe mysql: ")
+            mdp = getpass(prompt="Veuillez rentrer votre mot de passe mysql: ")
         elif(e.args[0] == 1062):
             print()
             print()
             print("La base de données est à jour")
-            print()
             break
         else:
             raise(e)
@@ -25,8 +25,7 @@ while(True):
 
 
 
-print("Voici la liste des graphes disponibles: ")
-print("1. Premier graphe")
+make_graph.list()
 print()
 print("Pour afficher un graphe, tapez 'i' avec i le numero du graphe")
 print("Pour réafficher la liste des graphes, tapez 'list' ")
@@ -36,7 +35,9 @@ print();print()
 while(True):
     print()
     cmd = input("Tapez votre commande: ")
-    if(cmd == 1):
-        make_graph.graph(mdp)
-    elif(cmd == 'quit'):
+    if(cmd == 'quit'):
         break
+    elif(cmd == 'list'):
+        make_graph.list()
+    else:
+        make_graph.graph(cmd,mdp)
--- Cassiopée/Code/make_graph.py
+++ Cassiopée/Code/make_graph.py
@@ -4,33 +4,40 @@ import pandas.io.sql as psql
 import matplotlib.pyplot as plt
 import tkinter
 
-def graph(mdp):
+def list():
+    print()
+    print('Voici la liste des graphes disponibles: ')
+    print("1. Premier graphe un peu nul pour l'instant")
+    print("2. Répartition des advisories par secteurs")
+
+def graph(i, mdp):
     db = mdb.Connection(host='localhost', db='cassiopee', passwd=mdp, user='root', charset='utf8')
 
 
-    query_sector = "select * from sector_instances"
 
-    df_sector = psql.read_sql(query_sector, con=db)
+    if(i == '1'):
+        query = "select date FROM cve"
 
-    df_sector.plot(x='sname',
-            y='quantity',
-            kind='bar',
-            title='Number of products per sector'
-            )
-    plt.show()
+        # Envoie resultat query dans un dataframe pandas
+        df = psql.read_sql(query, con=db)
 
-    db.close()
-    ###
-    query = "select date FROM cve"
+        # Convertit les string en dates numériques
+        df['date'] = df['date'].astype('datetime64[ns]')
 
-    # Envoie resultat query dans un dataframe pandas
-    df = psql.read_sql(query, con=db)
+        # Crée une colonne qui compte le nombre de CVE par date
+        df['count'] = df.date.map(df.groupby('date').size())
 
-    # Convertit les string en dates numériques
-    df['date'] = df['date'].astype('datetime64[ns]')
+        df.plot(x='date')
+        plt.show()
 
-    # Crée une colonne qui compte le nombre de CVE par date
-    df['count'] = df.date.map(df.groupby('date').size())
+    if(i == '2'):
+        query_sector = "select * from sector_instances"
 
-    df.plot(x='date')
-    plt.show()
+        df_sector = psql.read_sql(query_sector, con=db)
+
+        df_sector.plot(x='sname',
+                       y='quantity',
+                       kind='bar',
+                       title='Number of products per sector'
+                       )
+        plt.show()
